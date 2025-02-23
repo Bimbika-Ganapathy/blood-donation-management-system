02e9dd606e59ac384d64d0eed351a3498147704b
Bimbika-Ganapathy <123829698+Bimbika-Ganapathy@users.noreply.github.com>
2025-02-23 23:20:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+
+connectDB();
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+
+
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, () => {
+  console.log('server is running'
+  );
+});
